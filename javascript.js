f93023c112ad5f44f9435956819b2d835939f5f6
anjkrish2608 <64055313+anjkrish2608@users.noreply.github.com>
2020-05-01 14:22:21 +0930

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+button1.onclick = function(){
+    document.getElementById("box").style.height = "25px";
+};
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.color = "blue";
+
+});
+
+    <button id="button3">Fade</button>
+    <button id="button4">Reset</button>
